9e847aa234132d99b16e464b3f1e25cbf7b1155b
Aeden Jameson <aeden.jameson@gmail.com>
2020-05-15 09:03:46 -0700

    parameterize msk provision by environment

--- deploy/aws-msk.tf
+++ deploy/aws-msk.tf
@@ -1,3 +1,10 @@
+variable "environment" {}
+
+variable "kafka_version" {}
+variable "broker_type"   {}
+variable "volume_size"   {}
+
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -6,16 +13,29 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
+data "aws_security_group" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name = "group-name"
+    values = ["default"]
+  }
+}
+
 resource "aws_msk_cluster" "perch-photos" {
-  cluster_name           = "perch-photos-dev"
-  kafka_version          = "2.4.1"
+  cluster_name           = "perch-photos-${var.environment}"
+  kafka_version          = var.kafka_version
   number_of_broker_nodes = 3
 
   broker_node_group_info {
-    instance_type   = "kafka.t3.small"
-    ebs_volume_size = 20
+    instance_type   = var.broker_type
+    ebs_volume_size = var.volume_size
     client_subnets = slice(tolist(data.aws_subnet_ids.default.ids), 0, 3)
 
-    security_groups = ["sg-d6de2fb9"]
+    security_groups = [data.aws_security_group.default.id]
   }
 }
+
--- deploy/environments/dev.us-west-2.tfvars
+++ deploy/environments/dev.us-west-2.tfvars
@@ -1,1 +1,7 @@
+environment="nonprod"
+
 churps_bucket="perchphotos-churps-usw2-dev"
+kafka_version="2.4.1"
+broker_type="kafka.t3.small"
+volume_size=20
+
